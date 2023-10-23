0abf67dcc17fb39e22b80cfe7e3df1e4a0158bc2
henriqueR2023 <henriqueronald2023@gmail.com>
2023-10-22 21:45:23 -0400

    criando components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
+;
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    MenuBarComponent,
+    MenuTitleComponent,
+    BigCardComponent,
+    SmallCardComponent,
+    HomeComponent,
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
